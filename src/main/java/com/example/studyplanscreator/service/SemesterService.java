9edfba2fe31e043cc7ed76edea9c3dce3872de7b
mat-daemon <mateuszsmigielski40@gmail.com>
2023-01-09 12:12:18 +0100

    Semester management p1

---
+++ src/main/java/com/example/studyplanscreator/service/SemesterService.java
@@ -0,0 +1,26 @@
+package com.example.studyplanscreator.service;
+
+import com.example.studyplanscreator.model.Plan;
+import com.example.studyplanscreator.model.Semester;
+import com.example.studyplanscreator.repo.SemesterRepo;
+import com.example.studyplanscreator.repo.PlanRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SemesterService {
+    private final SemesterRepo repo;
+    private final PlanRepo planRepo;
+
+    public List<Semester> getAll() {
+        return repo.findAll();
+    }
+    public List<Semester> getSemestersFromPlan(long planId) {
+        Plan plan = planRepo.getPlanById(planId);
+        return repo.getSemestersByPlan(plan);
+    }
+
+}
