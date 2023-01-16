4c59721f43b87ba6c0751c31fd788608641bda2b
Palac2001 <stanislaw7766999@gmail.com>
2023-01-16 10:13:24 +0100

    Add semester view and searching view

---
+++ src/main/java/com/example/studyplanscreator/controller/SemesterController.java
@@ -0,0 +1,45 @@
+package com.example.studyplanscreator.controller;
+
+import com.example.studyplanscreator.model.*;
+import com.example.studyplanscreator.service.FacultyService;
+import com.example.studyplanscreator.service.SemesterService;
+import com.example.studyplanscreator.service.PlanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+public class SemesterController {
+    private final SemesterService semesterService;
+    private final PlanService planService;
+
+    @GetMapping("/semesters-in-plan")
+    private String semestersList(Model model, @RequestParam(required = false) Long planId) {
+        var foundSemesters = semesterService.getSemestersFromPlan(planId);
+        model.addAttribute("semesters", foundSemesters);
+        return "semesters/semesters-in-plan";
+    }
+
+    @GetMapping("/semester")
+    private String semester(Model model, @RequestParam(required = false) Long semesterId) {
+
+        return "semesters/semester";
+    }
+
+    @GetMapping("/view-classes")
+    private String viewClasses(Model model, @RequestParam(required = false) String class_name, @RequestParam(required = false) String class_type){
+
+        if(class_type != null)
+        {
+            return switch (class_type) {
+                case "course" -> "semesters/view-classes-courses";
+                case "group" -> "semesters/view-classes-courses";
+                case "module" -> "semesters/view-classes-modules";
+                default -> "semesters/view-classes";
+            };
+        }
+        return "semesters/view-classes";
+    }
+}
---
+++ src/main/java/com/example/studyplanscreator/repo/SemesterRepo.java
@@ -0,0 +1,14 @@
+package com.example.studyplanscreator.repo;
+
+import com.example.studyplanscreator.model.entity.Plan;
+import com.example.studyplanscreator.model.entity.Semester;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SemesterRepo extends JpaRepository<Semester, Long> {
+    List<Semester> getSemestersByPlan(Plan plan);
+    Semester getSemesterById(long id);
+}
