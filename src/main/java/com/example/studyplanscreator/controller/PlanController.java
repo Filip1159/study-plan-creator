106ff69729fd151533870e3460eac02f96da70de
Filip1159 <fwfw541111@gmail.com>
2023-01-17 00:49:22 +0100

    Add plans list with filters

--- src/main/java/com/example/studyplanscreator/controller/PlanController.java
+++ src/main/java/com/example/studyplanscreator/controller/PlanController.java
@@ -4,6 +4,7 @@ import com.example.studyplanscreator.model.entity.EducationLevel;
 import com.example.studyplanscreator.model.entity.Plan;
 import com.example.studyplanscreator.service.FacultyService;
 import com.example.studyplanscreator.service.PlanService;
+import com.example.studyplanscreator.service.TranslationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,18 +17,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class PlanController {
     private final PlanService planService;
-
     private final FacultyService facultyService;
-
+    private final TranslationService translationService;
 
     @GetMapping("/plans")
     private String plansList(Model model, @RequestParam(required = false) Long facultyId,
-                             @RequestParam(required = false) Long authorId,
+                             @RequestParam(required = false) String field,
+                             @RequestParam(required = false) String author,
                              @RequestParam(required = false) String level,
-                             @RequestParam(required = false) String academicYear,
-                             @RequestParam(required = false) String status) {
-        var foundPlans = planService.getPlansWithFilters(facultyId, authorId, EducationLevel.valueOf(level), academicYear, status);
+                             @RequestParam(required = false) String academicYear) {
+        var foundPlans = planService.getPlansWithFilters(facultyId, field, author, level == null ? null : EducationLevel.valueOf(level), academicYear);
         model.addAttribute("plans", foundPlans);
+        model.addAttribute("facultySelected", facultyId);
+        model.addAttribute("fieldTyped", field);
+        model.addAttribute("authorTyped", author);
+        model.addAttribute("levelSelected", level);
+        model.addAttribute("yearSelected", academicYear);
+        model.addAttribute("faculties", facultyService.getAll());
+        model.addAttribute("levels", translationService.readableNames(EducationLevel.values()));
         return "plans/plans-list";
     }
 
--- src/main/java/com/example/studyplanscreator/repo/PlanRepo.java
+++ src/main/java/com/example/studyplanscreator/repo/PlanRepo.java
@@ -5,14 +5,29 @@ import com.example.studyplanscreator.model.entity.Faculty;
 import com.example.studyplanscreator.model.entity.Plan;
 import com.example.studyplanscreator.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PlanRepo extends JpaRepository<Plan, Long> {
-    List<Plan> getPlansByAuthorsContainingAndFacultyAndLevelAndAcademicYear(
-            User author, Faculty faculty, EducationLevel level, String academicYear);
+
+    @Query(
+            "SELECT p FROM Plan p " +
+            "WHERE (:author is null or :author member of p.authors) " +
+            "AND (:faculty is null or :faculty = p.faculty) " +
+            "AND (:field is null or :field = p.field) " +
+            "AND (:level is null or :level = p.level) " +
+            "AND (:year is null or :year = p.academicYear)"
+    )
+    List<Plan> query(
+            @Param("author") User author,
+            @Param("faculty") Faculty faculty,
+            @Param("field") String field,
+            @Param("level") EducationLevel level,
+            @Param("year") String academicYear);
 
     Plan getPlanById(long id);
 }
